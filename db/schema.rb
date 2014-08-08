77bf134bd544bd3840899e9df802f80494ffca61
Matthew Vasseur <mattvasseur@gmail.com>
2014-08-08 18:42:26 -0400

    acts tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808223550) do
+ActiveRecord::Schema.define(version: 20140808223822) do
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
@@ -32,5 +32,12 @@ ActiveRecord::Schema.define(version: 20140808223550) do
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "age"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
