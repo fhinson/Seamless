f916136ad327a9d396a3a6667d4bef4e4e076abd
Matthew Vasseur <mattvasseur@gmail.com>
2014-08-08 18:58:09 -0400

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808223822) do
+ActiveRecord::Schema.define(version: 20140808225651) do
+
+  create_table "shoes", force: true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.string   "seller"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
