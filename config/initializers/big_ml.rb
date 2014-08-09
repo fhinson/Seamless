e17a59d5d463ade385174baa24adebc8811245b0
Matthew Vasseur <mattvasseur@gmail.com>
2014-08-09 15:12:10 -0400

    moved buttons

---
+++ config/initializers/big_ml.rb
@@ -0,0 +1,7 @@
+BigML.configure do |c|
+  c.username = 'MatthewVasseur'
+  c.api_key  = '7897dac38dbc1c1703e5ca60353e0216575d519c'
+  c.dev_mode = true
+  #c.debug   = true # Raises errors for bad requests
+end
+
